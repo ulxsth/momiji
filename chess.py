a1f8cd176293f017dca64431dfc0a6c3aa5a6588
ulxsth <ulxsth@gmail.com>
2023-01-15 18:42:18 +0900

    コマを移動するコマンドを実装した（/move <before_pos> <after_pos>)

--- chess.py
+++ chess.py
@@ -25,7 +25,8 @@ PEACE_MARKS = {
 }
 
 
-def create_board_state_text(board_state):
+board_state = START_BOARD_STATE
+def create_board_state_text():
     """board_stateから表示用のテキストを生成する
 
     Args:
@@ -56,3 +57,40 @@ def create_board_state_text(board_state):
 
     board_state_text += '```'
     return board_state_text
+
+
+def notation_to_pos(notation):
+    return ord(notation) - ord('a') + 1
+
+
+def txt_to_pos(pos_txt):
+    line = notation_to_pos(pos_txt[0])
+    column = int(pos_txt[1])
+    return column, line
+
+
+def pos_to_index(pos):
+    return list(map(lambda x: x-1, pos))
+
+
+def get_peace_by_pos(pos):
+    index = pos_to_index(pos)
+    return board_state[index[0]][index[1]]
+
+
+def set_peace_by_pos(peace, pos):
+    global board_state
+    index = pos_to_index(pos)
+    board_state[index[0]][index[1]] = peace
+
+
+def validate(before_pos_txt, after_pos_txt) -> bool:
+    return True
+
+
+def move(before_pos_txt, after_pos_txt):
+    before_pos = txt_to_pos(before_pos_txt)
+    after_pos = txt_to_pos(after_pos_txt)
+    peace = get_peace_by_pos(before_pos)
+    set_peace_by_pos(peace, after_pos)
+    set_peace_by_pos(' ', before_pos)
--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import chess
 intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
-is_playing_chess = False
-
 token: str
-with open('token.txt') as f:
+with open("token.txt", "r") as f:
     token = f.read()
 
+is_playing_chess = False
 
 @client.event
 async def on_ready():
@@ -27,10 +25,27 @@ async def on_message(message):
 
     # chess
     elif content == '/chess':
+        global is_playing_chess
         is_playing_chess = True
-        board_state = chess.START_BOARD_STATE
-        board_state_text = chess.create_board_state_text(board_state)
+        chess.board_state = chess.START_BOARD_STATE
+        board_state_text = chess.create_board_state_text()
         await channel.send('☕')
         await channel.send(board_state_text)
+        
+    if not is_playing_chess:
+        return
+    
+    # /move <before_pos> <after_pos>
+    if content.startswith('/move'):
+        before_pos_txt, after_pos_txt = content.split()[1:3]
+        # validate
+        if not chess.validate(before_pos_txt, after_pos_txt):
+            await channel.send("```>> その手は打てないよ！```")
+            return
+
+        chess.move(before_pos_txt, after_pos_txt)
+        board_state_text = chess.create_board_state_text()
+        await channel.send(board_state_text)
+
 
 client.run(token)
