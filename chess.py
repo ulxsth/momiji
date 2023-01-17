21ecaffed59a6fbd16e888827ca18c4b7dd368f4
ulxsth <ulxsth@gmail.com>
2023-01-17 12:03:01 +0900

    白も対応

--- chess.py
+++ chess.py
@@ -149,7 +149,7 @@ class Board():
         # 白    
         elif before_peace == 'P':
             # 縦1マス
-            if pos_diff == [-1, 0]:
+            if pos_diff == [-1, 0] and not self.is_exist(after_pos):
                 return True
             
             # 縦2マス（初動のみ）
