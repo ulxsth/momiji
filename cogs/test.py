2f2a96d68393515915eb91a6f8b0526017c6f201
ulxsth <ulxsth@gmail.com>
2024-02-19 19:42:33 +0900

    update: cogを呼び出すテスト

--- cogs/test.py
+++ cogs/test.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 
 class TestCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.cog.listener()
     async def on_message(self, message):
         content = message.content
--- main.py
+++ main.py
@@ -9,8 +9,16 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
+def load_cogs(bot):
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            cog = filename[:-3]
+            print(cog)
+            bot.load_extension(f'cogs.{cog}')
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+load_cogs(bot)
 bot.run(os.getenv('BOT_TOKEN'))
