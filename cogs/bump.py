a976b1cfaf3fdfc653abae34177fe2ec2b3275eb
ulxsth <ulxsth@gmail.com>
2024-02-20 17:35:08 +0900

    update: /bump のcd後に告知してくれるよう実装

---
+++ cogs/bump.py
@@ -0,0 +1,22 @@
+import asyncio
+from discord.ext import commands
+
+BUMP_COOLDOWN_MINUTE = 0.5
+
+
+class BumpCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content == '/bump':
+            await asyncio.sleep(BUMP_COOLDOWN_MINUTE * 60)
+            await message.channel.send("Time to bump!")
+
+
+async def setup(bot):
+    await bot.add_cog(BumpCog(bot))
